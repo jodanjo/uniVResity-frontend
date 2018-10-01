5d21b0660c5bbed925e9e164c6c3f56a3f0794bb
v5dan <vdanciu@csumb.edu>
2018-09-30 21:09:09 -0700

    updated CreateStream.js changed routing to react-router, added an Error.js route

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -36,7 +36,9 @@ import {
       .then(user => {
         if (user.id) {
           this.props.loadUser(user)
+          console.log(user.name);
           this.props.onRouteChange('home');
+          
         }
       })
   }
